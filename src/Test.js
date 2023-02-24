7bf8886d5a6da71bcfc99dde704476f6c2639cb4
Rishabh32700 <rishabhpep@gmail.com>
2023-02-24 13:46:22 +0530

    first commit

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback } from "react";
 import ReactFlow, {
   addEdge,
   useNodesState,
@@ -6,78 +6,98 @@ import ReactFlow, {
   Background,
   BackgroundVariant,
   ReactFlowProvider,
-  useStoreApi,
-} from 'reactflow';
+  useStoreApi
+} from "reactflow";
 
-import 'reactflow/dist/style.css';
-import './style.css';
+import "reactflow/dist/style.css";
+import "./style.css";
 
-import { initialEdges, initialNodes } from './nodes-and-edges';
+// import { initialEdges, initialNodes } from "./nodes-and-edges";
 
 const MIN_DISTANCE = 150;
 
 const Flow = () => {
-    const nodeDefaults = {
-        sourcePosition: Position.Right,
-        targetPosition: Position.Left,
-        style: {
-          borderRadius: '100%',
-          backgroundColor: '#fff',
-          width: 50,
-          height: 50,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      };
-
-    const initialNodes = [
-        {
-          id: '1',
-          position: { x: 0, y: 0 },
-          data: {
-            label: '⬛️',
-          },
-          ...nodeDefaults,
-        },
-        {
-          id: '2',
-          position: { x: 250, y: -100 },
-          data: {
-            label: '🟩',
-          },
-          ...nodeDefaults,
-        },
-        {
-          id: '3',
-          position: { x: 250, y: 100 },
-          data: {
-            label: '🟧',
-          },
-          ...nodeDefaults,
-        },
-        {
-          id: '4',
-          position: { x: 500, y: 0 },
-          data: {
-            label: '🟦',
-          },
-          ...nodeDefaults,
-        },
-      ];
+  const nodeDefaults = {
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+    style: {
+      borderRadius: '100%',
+      backgroundColor: '#fff',
+      width: 50,
+      height: 50,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  };
+  
+  const initialNodes = [
+    {
+      id: '1',
+      position: { x: 0, y: 0 },
+      data: {
+        label: '⬛️',
+      },
+      ...nodeDefaults,
+    },
+    {
+      id: '2',
+      position: { x: 250, y: -100 },
+      data: {
+        label: '🟩',
+      },
+      ...nodeDefaults,
+    },
+    {
+      id: '3',
+      position: { x: 250, y: 100 },
+      data: {
+        label: '🟧',
+      },
+      ...nodeDefaults,
+    },
+    {
+      id: '4',
+      position: { x: 500, y: 0 },
+      data: {
+        label: '🟦',
+      },
+      ...nodeDefaults,
+    },
+  ];
+  
+  const initialEdges = [
+    {
+      id: 'e1-2',
+      source: '1',
+      target: '2',
+    },
+    {
+      id: 'e1-3',
+      source: '1',
+      target: '3',
+    },
+  ];
   const store = useStoreApi();
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
 
   const getClosestEdge = useCallback((node) => {
     const { nodeInternals } = store.getState();
+
+    console.log("nodeInternals", nodeInternals, store);
     const storeNodes = Array.from(nodeInternals.values());
+    console.log("storeNodes",nodeInternals, nodeInternals.values(), storeNodes);
 
     const closestNode = storeNodes.reduce(
       (res, n) => {
         if (n.id !== node.id) {
+          console.log("node.id", node.id)
           const dx = n.positionAbsolute.x - node.positionAbsolute.x;
           const dy = n.positionAbsolute.y - node.positionAbsolute.y;
           const d = Math.sqrt(dx * dx + dy * dy);
@@ -87,12 +107,12 @@ const Flow = () => {
             res.node = n;
           }
         }
-
+        console.log("closestNode :: ", res);
         return res;
       },
       {
         distance: Number.MAX_VALUE,
-        node: null,
+        node: null
       }
     );
 
@@ -100,12 +120,13 @@ const Flow = () => {
       return null;
     }
 
-    const closeNodeIsSource = closestNode.node.positionAbsolute.x < node.positionAbsolute.x;
+    const closeNodeIsSource =
+      closestNode.node.positionAbsolute.x < node.positionAbsolute.x;
 
     return {
       id: `${node.id}-${closestNode.node.id}`,
       source: closeNodeIsSource ? closestNode.node.id : node.id,
-      target: closeNodeIsSource ? node.id : closestNode.node.id,
+      target: closeNodeIsSource ? node.id : closestNode.node.id
     };
   }, []);
 
@@ -114,13 +135,16 @@ const Flow = () => {
       const closeEdge = getClosestEdge(node);
 
       setEdges((es) => {
-        const nextEdges = es.filter((e) => e.className !== 'temp');
+        const nextEdges = es.filter((e) => e.className !== "temp");
 
         if (
           closeEdge &&
-          !nextEdges.find((ne) => ne.source === closeEdge.source && ne.target === closeEdge.target)
+          !nextEdges.find(
+            (ne) =>
+              ne.source === closeEdge.source && ne.target === closeEdge.target
+          )
         ) {
-          closeEdge.className = 'temp';
+          closeEdge.className = "temp";
           nextEdges.push(closeEdge);
         }
 
@@ -135,7 +159,7 @@ const Flow = () => {
       const closeEdge = getClosestEdge(node);
 
       setEdges((es) => {
-        const nextEdges = es.filter((e) => e.className !== 'temp');
+        const nextEdges = es.filter((e) => e.className !== "temp");
 
         if (closeEdge) {
           nextEdges.push(closeEdge);
